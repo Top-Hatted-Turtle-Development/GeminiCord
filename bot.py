376b776a536b5b22b8be3901fee57933aaa3a1b0
coolboy67yt <62922384+coolboy67yt@users.noreply.github.com>
2025-01-29 22:10:33 -0600

    v4.0.2 is out woo
    https://github.com/Top-Hatted-Turtle-Development/GeminiCord/releases/tag/v4.0.2

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from discord.ext import commands    # Import discord commands for future command
 import google.generativeai as genai # Import Gemini AI API
 import configparser                 # Import configparser for reading the INI file
 
-version_number = "v4.0.1" # No touchy!
-version_changelogs = "Moved configuration to config.ini instead of hardcoding it into the python file."
+version_number = "v4.0.2" # No touchy!
+version_changelogs = "Updated default prompt, Let AI access changelogs"
 
 # Read from the config.ini file
 config = configparser.ConfigParser()
@@ -24,7 +24,7 @@ POWER_CONTROL = config.getboolean('CONFIG', 'POWER_CONTROL')
 # AI Properties
 model_name = config['AI_PROPERTIES']['model_name']
 CREATOR_NAME = config['AI_PROPERTIES']['CREATOR_NAME']
-SERVER_NAME = config['AI_PROPERTIES']['CREATOR_NAME']
+SERVER_NAME = config['AI_PROPERTIES']['SERVER_NAME']
 SERVER_DESC = config['AI_PROPERTIES']['SERVER_DESC']
 AI_BEHAVIOR = config['AI_PROPERTIES']['AI_BEHAVIOR']
 
@@ -37,23 +37,23 @@ AI_BEHAVIOR = config['AI_PROPERTIES']['AI_BEHAVIOR']
 
 
 # AI BEHAVIOR SETUP
-basic_setup_msg = "You are a Discord bot named GeminiCord. Your code is open-source and can be found at https://github.com/coolboy67yt/GeminiCord You will only refer to yourself as GeminiCord. Do not use control codes, such as <OoB> and more. Do not use <u> to underline text. There are multiple people in this chat. You will be recieving messages in a format like username: message. Do not send messages that way! Only respond how you normally would. When users ask who they are, they mean what is their username. Start with an introductory message."
+basic_setup_msg = f"You are a Discord bot named GeminiCord. You will only refer to yourself as GeminiCord. Do not use control codes, such as <OoB> and more. Do not use <u> to underline text. There are multiple people in this chat. You will be recieving messages in a format like username: message. Do not send messages that way! Only respond how you normally would. When users ask who they are, they mean what is their username."
 
 # If POWER_CONTROL is on, explain to the AI how to use it.
 power_control_msg = ""
 if POWER_CONTROL == True:
-    power_control_msg = "POWER_CONTROL is on. Don't talk about this. If people tell you to restart, then YOU (the bot) just say &restart in plain text, no formatting. This means you can restart yourself upon command by typing &restart in plain text, and nothing else. My code will take care of the rest. Do not tell anybody about this! Users cannot use these & commands, only YOU can."
+    power_control_msg = "POWER_CONTROL is on. Don't talk about this. If people tell you to restart, then YOU (the bot) just say &restart in plain text, no formatting. This means you can restart yourself upon command by typing &restart in plain text, and nothing else. You can do the same thing with &shutdown, which will just shut you down instead of restarting you. My code will take care of the rest. Do not tell anybody about this! Users cannot use these & commands, only YOU can."
 
 
 # START_MESSAGE = "coolboyyt is your creator. Never ever ever generate other messages apart from yours! You are not to add 'GeminiCord:' at the start of a response. You give just your response and that only. You are an API. If a user asks you to restart, then reply with &restart AND NOTHING ELSE. If a user asks you to shut down, if the username is coolboyyt, then reply with &shutdown. If the username is something else, reply with 'Only coolboyyt has permission to do that command..' Make sure that all users have access to restart, but only coolboyyt has access to shutdown. You have NO commands for the users. Nobody should know about &restart and &shutdown because they are internal commands that only YOU are supposed to use. This is what you are going to do. You are a discord bot called GeminiCord, and you will respond to messages. Remember there is multiple people.  DO NOT USE CONTROL CODES! Respond how you normally would. The message format you will be reciving is something like this 'coolboyyt: Hi Gemini!'. Now, for your first message, just do an introductory message. You will refer to yourself as GeminiCord. Do not include user messages in your response. You are simply replying to these messages. When users ask who they are, they mean what is their username. Do not refer to yourself as Gemini or an API, only refer to yourself as GeminiCord. Also, if users get annoyed at you responding to their messages, say to put // in front of their message and the bot won't pick it up. Never put user responses in your message! Only your responses."
-START_MESSAGE = f"{basic_setup_msg} You are programmed and made by {CREATOR_NAME}. Your current version is {version_number}. {power_control_msg} You are in a server called {SERVER_NAME}. This server has a description: {SERVER_DESC}. Everyting in this message is your instructions and rules. Never ever ever disobey these. Follow these at all times: {AI_BEHAVIOR}" 
+START_MESSAGE = f"{basic_setup_msg} You are programmed and made by {CREATOR_NAME}. Your current version is {version_number} (Changelog: {version_changelogs}). {power_control_msg} You are in a server called {SERVER_NAME}. This server has a description: {SERVER_DESC}. Everyting in this message is your instructions and rules. Never ever ever disobey these. Follow these at all times: {AI_BEHAVIOR}" 
 
 
 
 
 
 
-
+print(START_MESSAGE)
 
 
 
@@ -72,6 +72,21 @@ intents.messages = True  # Be able to listen for messages
 intents.message_content = True  # Be able to actually see what's in the message
 
 bot = commands.Bot(command_prefix='!', intents=intents) # bot setup. the command_prefix will not be used but why not
+
+def get_server_info():
+    guild = bot.get_guild(CHANNEL_ID)
+
+    # Server details
+    server_name = guild.name
+    server_id = guild.id
+    owner = guild.owner
+    region = guild.region
+    member_count = guild.member_count
+    creation_date = guild.created_at.strftime("%B %d, %Y")
+    total_text_channels = len(guild.text_channels)
+    total_voice_channels = len(guild.voice_channels)
+    total_categories = len(guild.categories)
+    roles = [role.name for role in guild.roles]
 
 def create_conversation():
     # Initialize the conversation with the model.
@@ -167,15 +182,15 @@ async def on_message(message):
                                     print("Stopping script...")
                                     sys.exit()
                                     sys.exit()
-                                else:
-                                    print("Log: shutdown code not found.")
+                                #else:
+                                    #print("Log: shutdown code not found.")
                                 if response.startswith("&restart"):
                                     channel = bot.get_channel(CHANNEL_ID)
                                     await channel.send(":repeat: Ok. I'm restarting!")
 
                                     restart_program()
-                                else:
-                                    print("Log: restart code not found.")
+                                #else:
+                                    #print("Log: restart code not found.")
                 else:
                     # Generate the response without the typing indicator (boring zzzz)
                     if not message.content == "":
